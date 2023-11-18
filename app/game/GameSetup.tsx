189e853ea6db9325934b92a4d5a305b22d89bbd2
pride829 <pride829@gmail.com>
2023-11-18 22:01:28 +0800

    Add a checking of if all name fields are filled

--- app/game/GameSetup.tsx
+++ app/game/GameSetup.tsx
@@ -3,22 +3,30 @@ import PlayerList from './PlayerList';
 import { useGameContext } from './GameContext';
 
 function GameSetup(onSubmit) {
-    const { MIN_PLAYERS, MAX_PLAYERS, numberOfPlayers, setNumberOfPlayers = () => { } } =
+    const { MIN_PLAYERS, MAX_PLAYERS, numberOfPlayers, setNumberOfPlayers = () => { }, playerNames } =
         useGameContext() ?? {
             MIN_PLAYERS: 0,
             MAX_PLAYERS: 0,
             numberOfPlayers: 0,
-            setNumberOfPlayers: undefined
+            setNumberOfPlayers: undefined,
+            playerNames: []
         };
 
-    const handleInputChange = event => {
+    const handlePlayerCountChange = event => {
         const inputNumber = parseInt(event.target.value, 10);
         setNumberOfPlayers(isNaN(inputNumber) ? 0 : inputNumber);
     };
 
     const handleFormSubmit = event => {
         event.preventDefault();
-        onSubmit(numberOfPlayers);
+        // Check if the first numberOfPlayers elements are filled.
+        const isFormValid = Object.values(playerNames).slice(0, numberOfPlayers).every((value) => value !== '');
+
+        if (!isFormValid) {
+            alert("Please fill in all fields");
+        } else {
+            onSubmit();
+        }
     };
 
     const playerOptions = Array.from({ length: MAX_PLAYERS - MIN_PLAYERS + 1 }, (_, index) => MIN_PLAYERS + index);
@@ -28,7 +36,7 @@ function GameSetup(onSubmit) {
             <h2>Game Setup</h2>
             <form onSubmit={handleFormSubmit}>
                 Enter the number of players:
-                <select id="playerCount" value={numberOfPlayers} onChange={handleInputChange}>
+                <select id="playerCount" value={numberOfPlayers} onChange={handlePlayerCountChange}>
                     {playerOptions.map((count) => (
                         <option key={count} value={count}>
                             {count}
@@ -36,6 +44,7 @@ function GameSetup(onSubmit) {
                     ))}
                 </select>
                 <PlayerList numberOfPlayers={numberOfPlayers} />
+                <button type="submit">Submit</button>
             </form>
         </>
     );
